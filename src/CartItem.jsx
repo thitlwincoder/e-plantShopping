7e78090cb8ad9e2ca67c3f1b0eddc287f15500cc
christian-schw <Christianschwanse@gmx.de>
2024-10-07 10:41:08 -0400

    Add handleCheckoutShopping in CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -22,7 +22,9 @@ const CartItem = ({ onContinueShopping }) => {
         onContinueShopping(e);
     };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
     const handleIncrement = (item) => {
     };
@@ -63,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
     );
