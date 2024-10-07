5d117ae952d3a46759e16db3ee6fc3576deb8e6c
christian-schw <Christianschwanse@gmx.de>
2024-10-07 11:17:57 -0400

    Implement handleRemove in CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -48,6 +48,7 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
     };
 
     // Calculate total cost based on quantity for an item
